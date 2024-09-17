b29c59cf9c1917adff6a195a9ad780778b6e60e1
Juan Ferrera Sala <tostyfis360@gmail.com>
2024-09-17 14:11:36 +0200

    Initial commit with basic calculator functionality

---
+++ src/main/java/com/example/BasicCalculator.java
@@ -0,0 +1,53 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        
+        double numer1 = 3.6;
+
+        double numer2 = 4;
+        
+        System.out.println("Hello, this is the add operation!");
+        
+        double resAdd = add(numer1, numer2);
+
+        System.out.println("Operation: " + numer1 + " + " + numer2 + " = " + resAdd);
+
+        System.out.println("Hello, this is the subtract operation!");
+        
+        double resSubtract = subtract(numer1, numer2);
+
+        System.out.println("Operation: " + numer1 + " - " + numer2 + " = " + resSubtract);
+
+        System.out.println("Hello, this is the multiply operation!");
+
+        double resMultiply = multiply(numer1, numer2);
+
+        System.out.println("Operation: " + numer1 + " * " + numer2 + " = " + resMultiply);
+
+        System.out.println("Hello, this is the divide operation!");
+
+        double resDivide = divide(numer1, numer2);
+
+        System.out.println("Operation: " + numer1 + " / " + numer2 + " = " + resDivide);
+    
+    }
+
+    public static double add(double a, double b) {
+        return a+b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a-b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a*b;
+    }
+
+    public static double divide(double a, double b) {
+        return a/b;
+    }
+}
